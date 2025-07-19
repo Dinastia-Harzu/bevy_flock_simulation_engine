bb5f6ee4e992854ac83d2c6a85620e526f3eed22
Dinastía Harzu αγρ <dinastiadelah@gmail.com>
2025-07-20 00:07:05 +0200

    Corregir error pequeño

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,9 @@ pub struct BogStandardPlugin;
 impl Plugin for BogStandardPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<AppState>()
-            .add_systems(Startup, setup)
             .insert_resource(Time::<Fixed>::from_hz(60.0))
             .insert_resource(ClearColor(Color::srgba(0.0, 0.0, 0.0, 1.0)))
+            .add_systems(Startup, setup)
             .add_systems(Update, common_input)
             .add_systems(OnEnter(AppState::Finished), exit);
     }
